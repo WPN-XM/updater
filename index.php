56867eb98fa15af8de354fa03886a69c83047af4
jakoch <jakoch@web.de>
2012-11-03 17:54:53 +0100

    show some links to the scripts

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<?php header("HTTP/1.0 404 Not Found");
-//header('Location: checkversion.php');
+<?php // :) ?>
+<a href="registry-status.php">Software Components Registry - Status</a>
+<a href="registry-update.php">Software Components Registry - Update</a>
+<a href="generate-download-list.php">Generate new Download list of Software Components for the All-In-One Installer</a>
+<a href="update-website.php">Update the Version Numbers on the website</a>

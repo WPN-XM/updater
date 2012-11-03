25d50a4a9505f9f8f33ff44836d84a81fafd63a9
jakoch <jakoch@web.de>
2012-11-03 17:56:02 +0100

    some breaks

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php // :) ?>
+
 <a href="registry-status.php">Software Components Registry - Status</a>
+<br>
 <a href="registry-update.php">Software Components Registry - Update</a>
+<br>
 <a href="generate-download-list.php">Generate new Download list of Software Components for the All-In-One Installer</a>
+<br>
 <a href="update-website.php">Update the Version Numbers on the website</a>

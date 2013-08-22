7588965e5ae119661a0973be80bb629fb32d9af4
jakoch <jakoch@web.de>
2013-08-22 15:26:04 +0200

    bugfix for postgresql version crawler (to recognize rc version)

--- registry-update.php
+++ registry-update.php
@@ -377,18 +377,29 @@ function get_latest_version_of_postgresql()
     $crawler = $goutte_client->request('GET', 'http://www.enterprisedb.com/products-services-training/pgbindownload');
 
     return $crawler->filterXPath('//p/i')->each( function ($node, $i) use ($registry) {
-        //echo $node->text(); // = Binaries from installer version 9.3.0 Beta2
+        //echo $node->text(); // = "Binaries from installer version 9.3.0 Beta2" or "Binaries from installer version 9.3.0 Release Candidate 1"
         $value = strtolower($node->text());
 
-        if (preg_match("#(\d+\.\d+(\.\d+)*)(?:(\s)(beta|b|rc|alpha|a|patch|pl|p)?(\d+))?#", $value, $matches)) {
+        if (preg_match("#(\d+\.\d+(\.\d+)*)(?:[ ])(release candidate|beta|b|rc|alpha|a|patch|pl|p)(?:[ ])(\d+)?#i", $value, $matches)) {
 
-            if(isset($matches[4]) === true) { // if, we have " beta2" something after the version number
+            #var_dump($matches);
+
+            if(isset($matches[3]) === true) { // if we have "release candidate" or "beta"
+                $version = $matches[1];
+                $pre_release_version = $matches[4];
+                if($matches[3] === 'release candidate') {
+                    $download_version = $version . '-rc' . $pre_release_version;
+                } 
+            } /*elseif(isset($matches[4]) === true) { // if we have " beta2" something after the version number
                 $version = str_replace(' ', '-', $matches[0]); // turn "9.3.0 beta2" into "9.3.0-beta2"
                 $download_version = $version;
-            } else {
+            }*/ else {
                 $version = $matches[0]; // just 1.2.3
-                $download_version = $version . '-1'; // wtf? "-1" means not beta or what?
+                $download_version = $version . '-1'; // wtf? "-1" means "not beta", or what?
             }
+
+            #var_dump($download_version);
+            #var_dump($version);
 
             if (version_compare($version, $registry['postgresql']['latest']['version'], '>=')) {
                 return array(

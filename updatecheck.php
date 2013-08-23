f43a43b2977c0fb334b94141bc7634e8f6cc1b38
jakoch <jakoch@web.de>
2013-08-23 21:04:42 +0200

    added "?s=all", returning all software components with name/website/latestversion as JSON

--- updatecheck.php
+++ updatecheck.php
@@ -31,9 +31,11 @@
  * Update Check - Response
  * -----------------------
  * The script provides a json response to a update-check request
- * for individual components of the WPN-XM Server Stack.
+ * for individual or all components of the WPN-XM Server Stack.
  *
- * Example request: updatecheck.php?s=nginx&v=1.2.1
+ * Example request: 
+ * 1) updatecheck.php?s=nginx&v=1.2.1
+ * 2) updatecheck.php?s=all
  */
 
 // load software components registry
@@ -50,6 +52,22 @@ $s = filter_input(INPUT_GET, 's', FILTER_SANITIZE_STRING);
 $v = filter_input(INPUT_GET, 'v', FILTER_SANITIZE_STRING);
 // fallback, if no version was set - this makes requests without "v" parameter possible
 $v = (!empty($v)) ? $v : '0.0.0';
+
+// request all software components with name/website/latestversion as JSON
+if (!empty($s) && $s === 'all') {
+
+    // reduce the registry (drop all version numbers and their URLs, but keep name/website/latestversion)
+    $data = array();
+    foreach($registry as $software => $details) {
+        $data[$software]['name'] = isset($details['name']) ? $details['name'] : '';
+        $data[$software]['website'] =  isset($details['website']) ? $details['website'] : '';
+        $data[$software]['latest'] = $details['latest'];
+    }
+
+    // send response as json message
+    echo json_encode($data);
+    exit(0);
+}
 
 // does the requested software exist in our registry?
 if (!empty($s) && array_key_exists($s, $registry) ) {

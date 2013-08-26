9cbb12cf1d9acf42c1b248edec4bcde772459146
jakoch <jakoch@web.de>
2013-08-26 15:49:20 +0200

    typo

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -568,7 +568,7 @@
     '10.0' => 'http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe',
     'latest' => 
     array (
-        'version' => '10.0'
+        'version' => '10.0',
         'url' => 'http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe',
     ),
   ),

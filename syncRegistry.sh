67afda403ba5cd2870d553bb9e6ad6df4f413dce
jakoch <jakoch@web.de>
2013-10-29 00:20:12 +0100

    renamed 1,2 to source, target

--- syncRegistry.sh
+++ syncRegistry.sh
@@ -4,12 +4,12 @@
 
 # Adjust folder names
 #SOURCE_FILE1=/home/.../wpnxm/updater/wpnxm-software-registry.php
-#TARGET_FILE2=/var/www/.../wpnxm/wpnxm-software-registry.php
+#TARGET_FILE1=/var/www/.../wpnxm/wpnxm-software-registry.php
 
-if [ $SOURCE_FILE1 -nt $TARGET_FILE2 ]; then
-  #echo "File 1 is newer than file 2"
-  cp -u $SOURCE_FILE1 $TARGET_FILE2
+if [ $SOURCE_FILE1 -nt $TARGET_FILE1 ]; then
+  #echo "Source File newer than Target File."
+  cp -u $SOURCE_FILE1 $TARGET_FILE1
 else
-  #echo "File 1 is older than file 2"
+  #echo "Source File older than Target File."
    echo "No registry update necessary. The repository file is older than the file in the webfolder."
 fi

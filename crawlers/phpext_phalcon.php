bd8e3c0253d09526e0db29caff3d145f66d0af3f
jakoch <jakoch@web.de>
2014-04-19 17:45:53 +0200

    added crawler for PHP Extensions Phalcon, issue WPN-XM/WPN-XM#193

---
+++ crawlers/phpext_phalcon.php
@@ -0,0 +1,67 @@
+<?php
+   /**
+    * WPИ-XM Server Stack
+    * Jens-André Koch © 2010 - onwards
+    * http://wpn-xm.org/
+    *
+    *        _\|/_
+    *        (o o)
+    +-----oOO-{_}-OOo------------------------------------------------------------------+
+    |                                                                                  |
+    |    LICENSE                                                                       |
+    |                                                                                  |
+    |    WPИ-XM Serverstack is free software; you can redistribute it and/or modify    |
+    |    it under the terms of the GNU General Public License as published by          |
+    |    the Free Software Foundation; either version 2 of the License, or             |
+    |    (at your option) any later version.                                           |
+    |                                                                                  |
+    |    WPИ-XM Serverstack is distributed in the hope that it will be useful,         |
+    |    but WITHOUT ANY WARRANTY; without even the implied warranty of                |
+    |    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 |
+    |    GNU General Public License for more details.                                  |
+    |                                                                                  |
+    |    You should have received a copy of the GNU General Public License             |
+    |    along with this program; if not, write to the Free Software                   |
+    |    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA    |
+    |                                                                                  |
+    +----------------------------------------------------------------------------------+
+    */
+
+namespace WPNXM\Updater\Crawler;
+
+/**
+ * Phalcon - Version Crawler
+ */
+class phpext_phalcon extends VersionCrawler
+{
+    /**
+     * http://phalconphp.com/en/download/windows
+     * http://static.phalconphp.com/files/
+     */
+    public $url = 'http://static.phalconphp.com/files/';
+
+    public $needsOnlyRegistrySubset = false;
+
+    public $needsGuzzle = true;
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+
+            // there are "rc" versions, but we don't take them into account
+            if (preg_match("#php5.4.0_(\d+\.\d+(\.\d+)*)#i", $node->text(), $matches))
+            {
+                $version = $matches[1];
+
+                if (version_compare($version, $this->registry['phpext_xcache']['latest']['version'], '>='))
+                {
+                    // http://static.phalconphp.com/files/phalcon_x86_VC9_php5.4.0_1.3.1_nts.zip
+                    return array(
+                        'version' => $version,
+                        'url' => 'http://static.phalconphp.com/files/phalcon_x86_VC9_php5.4.0_'.$version.'_nts.zip'
+                    );
+                }
+            }
+        });
+    }
+}

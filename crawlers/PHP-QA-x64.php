37c8063a15a761fb33ef19f68b76af8146ad20bc
jakoch <jakoch@web.de>
2014-08-26 17:55:16 +0200

    added crawlers for PHP QA versions

---
+++ crawlers/PHP-QA-x64.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2014 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+/**
+ * PHP QA x64- Version Crawler
+ */
+class PHP_QA_X64 extends VersionCrawler
+{
+    public $name = 'php-qa-x64';
+
+    public $url = 'http://windows.php.net/downloads/qa/';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            echo $node->text() . "<br>";
+            if (preg_match("#php-(\d+\.\d+(\.\d+)*(RC\d+))-nts-Win32-VC11-x64.zip$#", $node->text(), $matches)) {
+                if (version_compare($matches[1], $this->registry['php-x64']['latest']['version'], '>=')) {
+                    return array(
+                        'version' => $matches[1],
+                        'url' => 'http://windows.php.net/downloads/qa/' . $node->text()
+                    );
+                }
+            }
+        });
+    }
+
+    /**
+     * PHP release files are moved from "/releases" to "/releases/archives", with every new version.
+     * That means, latest version must point to "/releases".
+     * Every other version points to "/releases/archives".
+     */
+    public function modifyRegistry($registry)
+    {
+        foreach ($registry['php-x64'] as $version => $url) {
+            // do not modify array key "latest"
+            if( $version === 'latest') continue;
+            // do not modify array key with latest version number - it must point to "/releases".
+            if( $version === $registry['php-qa-x64']['latest']['version']) continue;
+            // replace the path on any other version
+            $new_url = str_replace('php.net/downloads/qa', 'php.net/downloads/qa/archives', $url);
+            // insert at old array position, overwriting the old url
+            $registry['php-x64'][$version] = $new_url;
+        }
+
+        return $registry;
+    }
+}
---
+++ crawlers/PHP-QA-x86.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2014 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+/**
+ * PHP QA x86 - Version Crawler
+ */
+class PHP_QA_X86 extends VersionCrawler
+{
+    public $name = 'php-qa';
+
+    public $url = 'http://windows.php.net/downloads/qa/';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#php-(\d+\.\d+(\.\d+)(RC\d+)*)-nts-Win32-VC11-x86.zip$#", $node->text(), $matches)) {
+                if (version_compare($matches[1], $this->registry['php']['latest']['version'], '>=')) {
+                    return array(
+                        'version' => $matches[1],
+                        'url' => 'http://windows.php.net/downloads/qa/' . $node->text()
+                    );
+                }
+            }
+        });
+    }
+
+    /**
+     * PHP release files are moved from "/releases" to "/releases/archives", with every new version.
+     * That means, latest version must point to "/releases".
+     * Every other version points to "/releases/archives".
+     */
+    public function modifyRegistry($registry)
+    {
+        foreach ($registry['php'] as $version => $url) {
+            // do not modify array key "latest"
+            if( $version === 'latest') continue;
+            // do not modify array key with latest version number - it must point to "/releases".
+            if( $version === $registry['php-qa']['latest']['version']) continue;
+            // replace the path on any other version
+            $new_url = str_replace('php.net/downloads/qa', 'php.net/downloads/qa/archives', $url);
+            // insert at old array position, overwriting the old url
+            $registry['php'][$version] = $new_url;
+        }
+
+        return $registry;
+    }
+}

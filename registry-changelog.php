20ff097d941d15f5fa675cf4f9bd5abd9be70d60
jakoch <jakoch@web.de>
2014-09-24 00:30:17 +0200

    cleanup

--- registry-changelog.php
+++ registry-changelog.php
@@ -23,65 +23,62 @@ echo getChangelog('bigpack-0.7.0-w32', 'full-0.8.0-php5.4-w32');
  */
 function diffRegistries($registryA, $registryB)
 {
-	$a = json_decode($registryA, true);
-	$b = json_decode($registryB, true);
+    $a = json_decode($registryA, true);
+    $b = json_decode($registryB, true);
 
-	$a = reindexArrayComponentNamed($a);
-	$b = reindexArrayComponentNamed($b);
+    $a = reindexArrayComponentNamed($a);
+    $b = reindexArrayComponentNamed($b);
 
-	$diff = array();
+    $diff = array();
 
-	foreach($a as $component => $values)
-    {
-    	// difference entry: version A does no longer exist in B (removed).
-    	if(array_key_exists($component, $b) === false) {
-           	$diff[$component] = 'DEL ' . $component . ' was removed';
-    		continue;
-    	}
+    foreach ($a as $component => $values) {
+        // difference entry: version A does no longer exist in B (removed).
+        if (array_key_exists($component, $b) === false) {
+            $diff[$component] = 'DEL ' . $component . ' was removed';
+            continue;
+        }
 
-    	$versionA = $values[3];
-    	$versionB = $b[$component][3];
+        $versionA = $values[3];
+        $versionB = $b[$component][3];
 
-    	// version comparison
+        // version comparison
+        #echo 'Comparing ' . $component . ': Version A ' . $versionA . ' with Version B ' . $versionB . '<br>';
 
-    	#echo 'Comparing ' . $component . ': Version A ' . $versionA . ' with Version B ' . $versionB . '<br>';
-
-		$vcResult = version_compare($versionA, $versionB);
+        $vcResult = version_compare($versionA, $versionB);
 
         // Version A lower then Version B
-		if($vcResult === -1) {
-			$diff[$component] = 'UPD ' . $component . ' was updated from v' . $versionA . ' to v' . $versionB;
-		}
+        if ($vcResult === -1) {
+            $diff[$component] = 'UPD ' . $component . ' was updated from v' . $versionA . ' to v' . $versionB;
+        }
 
-		// Version A equals Version B
-		elseif($vcResult === 0) {
-			// do nothing
-			//$diff[$component] = '';
-		}
+        // Version A equals Version B
+        elseif ($vcResult === 0) {
+            // do nothing
+            //$diff[$component] = '';
+        }
 
-		/**
-		 * Version A higher Version B
-		 *
-		 * this shouldn't happen at all.
-		 * this would mean, that we use a lower component version in a higher installer version .. o.O
-		 * the only use case might be, to downgrade to fix a version incompatability.
-		 */
-		elseif($vcResult === 1) {
-			$diff[$component] = 'UPD ' . $component . ' was downgraded from v' . $versionA . ' to v' . $versionB;
-		}
+        /**
+         * Version A higher Version B
+         *
+         * this shouldn't happen at all.
+         * this would mean, that we use a lower component version in a higher installer version .. o.O
+         * the only use case might be, to downgrade to fix a version incompatability.
+         */ 
+        elseif ($vcResult === 1) {
+            $diff[$component] = 'UPD ' . $component . ' was downgraded from v' . $versionA . ' to v' . $versionB;
+        }
 
-    	unset($versionA, $versionB);
+        unset($versionA, $versionB);
     }
 
-    foreach($b as $component => $values)
-    {
-    	// difference entry: B contains a component which is not in A (added).
-    	if(array_key_exists($component, $a) === false) {
-           	$diff[$component] = 'ADD ' . $component . ' v' . $values[3] . ' was added';
-    	}
+    foreach ($b as $component => $values) {
+        // difference entry: B contains a component which is not in A (added).
+        if (array_key_exists($component, $a) === false) {
+            $diff[$component] = 'ADD ' . $component . ' v' . $values[3] . ' was added';
+        }
     }
 
-	return $diff;
+    return $diff;
 }
 
 /**
@@ -89,20 +86,19 @@ function diffRegistries($registryA, $registryB)
  */
 function reindexArrayComponentNamed($array)
 {
-	$result = array();
-	foreach($array as $key => $values)
-    {
-    	$result[$values[0]] = $values;
+    $result = array();
+    foreach ($array as $key => $values) {
+        $result[$values[0]] = $values;
     }
-	return $result;
+    return $result;
 }
 
 function getChangelog($registryNameA, $registryNameB)
 {
-	$diff = diffRegistries(
-		file_get_contents(__DIR__ . '/registry/' . $registryNameA . '.json'),
-		file_get_contents(__DIR__ . '/registry/' . $registryNameB . '.json')
-	);
+    $diff = diffRegistries(
+        file_get_contents(__DIR__ . '/registry/' . $registryNameA . '.json'), 
+        file_get_contents(__DIR__ . '/registry/' . $registryNameB . '.json')
+    );
 
-	return "<pre>" . implode("\n", $diff) . "</pre>";
+    return "<pre>" . implode("\n", $diff) . "</pre>";
 }

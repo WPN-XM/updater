6d7551ed238f67ad4fc4019ec3fd4d4855a889f3
jakoch <jakoch@web.de>
2014-09-29 21:57:08 +0200

    pulled downloadFilenames out into own file

--- registry-update.php
+++ registry-update.php
@@ -217,59 +217,7 @@ if (isset($action) && $action === 'update-installer-registry') {
 
   $file = __DIR__ . '\registry\\' . $installer . '.json';
 
-  $downloadFilenames = array(
-    'adminer' => 'adminer.php', // ! php file
-    'closure-compiler' => 'closure-compiler.zip',
-    'composer' => 'composer.phar', // ! phar file
-    'imagick' => 'imagick.zip',
-    'junction' => 'junction.zip',
-    'mariadb' => 'mariadb.zip',
-    'memadmin' => 'memadmin.zip',
-    'memcached' => 'memcached.zip',
-    'mongodb' => 'mongodb.zip',
-    'nginx' => 'nginx.zip',
-    'node' =>  'node.exe', // ! exe file
-    'nodenpm' => 'nodenpm.zip',
-    'openssl' =>  'openssl.exe', // ! exe file
-    'pear' => 'go-pear.phar', // ! phar file
-    'perl' =>  'perl.zip',
-    'php' => 'php.zip',
-    'php-qa' => 'php.zip',
-    'php-qa-x64' => 'php.zip',
-    'php-x64' => 'php.zip',
-    'phpext_amqp' => 'phpext_amqp.zip',
-    'phpext_apc' => 'phpext_apc.zip',
-    'phpext_apcu' => 'phpext_apcu.zip',
-    'phpext_imagick' => 'phpext_imagick.zip',
-    'phpext_mailparse' => 'phpext_mailparse.zip',
-    'phpext_memcache' => 'phpext_memcache.zip', // without D
-    'phpext_mongo' => 'phpext_mongo.zip',
-    'phpext_msgpack' => 'phpext_msgpack.zip',
-    'phpext_phalcon' => 'phpext_phalcon.zip',
-    'phpext_rar' => 'phpext_rar.zip',
-    'phpext_trader' => 'phpext_trader.zip',
-    'phpext_uploadprogress' => 'phpext_uploadprogress.zip',
-    'phpext_uprofiler' => 'phpext_uprofiler.zip',
-    'phpext_varnish' => 'phpext_varnish.zip',
-    'phpext_wincache' => 'phpext_wincache.exe', // ! exe file
-    'phpext_xcache' => 'phpext_xcache.zip',
-    'phpext_xdebug' => 'phpext_xdebug.dll', // ! dll file
-    'phpext_xhprof' => 'phpext_xhprof.zip',
-    'phpext_zmq' => 'phpext_zmq.zip',
-    'phpmemcachedadmin' => 'phpmemcachedadmin.zip',
-    'phpmyadmin' =>  'phpmyadmin.zip',
-    'pickle' => 'pickle.phar',
-    'postgresql' =>  'postgresql.zip',
-    'redis' =>  'redis.zip',
-    'rockmongo' =>  'rockmongo.zip',
-    'sendmail' =>  'sendmail.zip',
-    'uprofiler' =>  'uprofiler.zip',
-    'varnish' =>  'varnish.zip',
-    // vcredist_x86.exe (do not delete this comment, its for easier comparison with the .iss file)
-    'webgrind' =>  'webgrind.zip',
-    'wpnxmscp' =>  'wpnxmscp.zip',
-    'xhprof' =>  'xhprof.zip',
-  );
+  $downloadFilenames = include __DIR__ . '\downloadFilenames.php';
 
   $data = array();
 

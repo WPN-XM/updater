d37c67732e77624ed7ee4f158bbb678c072f7ee7
jakoch <jakoch@web.de>
2014-09-29 18:25:42 +0200

    added MongoDb x86_64 crawler, issue https://github.com/WPN-XM/WPN-XM/issues/255

---
+++ crawlers/mongodb-x64.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2014 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+/**
+ * MongoDb (x86_64) - Version Crawler
+ */
+class mongodb extends VersionCrawler
+{
+    // formerly http://www.mongodb.org/downloads
+    public $url = 'http://dl.mongodb.org/dl/win32/';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            // no RC versions!
+            if (preg_match("#win32-x86_64-(\d+\.\d+(\.\d+)*).zip$#", $node->attr('href'), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['mongodb']['latest']['version'], '>=')) {
+                    return array(
+                        'version' => $version,
+                        # http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2.2.0.zip
+                        'url' => 'http://downloads.mongodb.org/win32/mongodb-win32-x86_64-' . $version . '.zip'
+                    );
+                }
+            }
+        });
+    }
+}

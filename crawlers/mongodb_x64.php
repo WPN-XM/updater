862a6a2cc5f9769a5d26bdb2e4fd741b9692cfb0
jakoch <jakoch@web.de>
2014-10-09 15:10:35 +0200

    witch from 64-bit legacy to 64-bit 2008 plus mongodb-win32-x86_64-2008plus-, isse https://github.com/WPN-XM/WPN-XM/issues/283

--- crawlers/mongodb_x64.php
+++ crawlers/mongodb_x64.php
@@ -24,14 +24,21 @@ class mongodb_x64 extends VersionCrawler
     public function crawlVersion()
     {
         return $this->filter('a')->each(function ($node) {
-            // no RC versions!
-            if (preg_match("#win32-x86_64-(\d+\.\d+(\.\d+)*).zip$#", $node->attr('href'), $matches)) {
+
+            /**
+             * 1. We don't take RC versions into account
+             * 2. RegExp switched from "64-bit legacy" (v0.9.0) to "64-bit 2008 plus" (v0.9.1)
+             *    The 64-bit legacy build lacks newer features of Windows that enhance performance.
+             *    Use legacy build on Windows Server 2003, 2008, or Windows Vista.
+             *    Legacy RegExp: "mongodb-win32-x86_64-"
+             */
+            if (preg_match("#mongodb-win32-x86_64-2008plus-(\d+\.\d+(\.\d+)*).zip$#", $node->attr('href'), $matches)) {
                 $version = $matches[1];
                 if (version_compare($version, $this->registry['mongodb-x64']['latest']['version'], '>=')) {
                     return array(
                         'version' => $version,
-                        # http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2.2.0.zip
-                        'url' => 'http://downloads.mongodb.org/win32/mongodb-win32-x86_64-' . $version . '.zip'
+                        # http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.2.0.zip
+                        'url' => 'http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-' . $version . '.zip'
                     );
                 }
             }

c85ecdb547cb92cc90e99d61b1d198d3ce85a1b2
jakoch <jakoch@web.de>
2014-10-30 14:11:59 +0100

    fix for ini_set (true of type boolean, but the function expects a string)

--- registry-update.php
+++ registry-update.php
@@ -12,7 +12,7 @@ $start = microtime(true);
 set_time_limit(180); // 60*3
 date_default_timezone_set('UTC');
 error_reporting(E_ALL);
-ini_set('display_errors', true);
+ini_set('display_errors', '1');
 
 if (!extension_loaded('curl')) {
     exit('Error: PHP Extension cURL required.');

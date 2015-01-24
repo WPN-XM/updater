ce5a092b9c2abdf77c5b06b16c2a92987f2c0bee
jakoch <jakoch@web.de>
2015-01-24 02:25:47 +0100

    fix openssl matcher to reflect the changes of the provider

--- crawlers/openssl.php
+++ crawlers/openssl.php
@@ -20,14 +20,26 @@ class openssl extends VersionCrawler
     public function crawlVersion()
     {
         return $this->filter('a')->each( function ($node) {
-            // http://slproweb.com/download/Win32OpenSSL_Light-1_0_1d.exe
-            if (preg_match("#Win32OpenSSL_Light-(\d+\_\d+\_\d+[a-z]).exe$#", $node->attr('href'), $matches)) {
-                // the version match contains underscores: so turn "1_0_1d" into "1.0.1d", that's still not SemVer but anyway
-                $version = str_replace('_', '.', $matches[1]);
+
+            /**
+             * The regexp must take the following cases into account:
+             *
+             * http://slproweb.com/download/Win32OpenSSL_Light-1_0_1d.exe  - one char lowercase?
+             * http://slproweb.com/download/Win32OpenSSL_Light-1_0_1L.exe  - one char uppercase ?
+             * http://slproweb.com/download/Win32OpenSSL_Light-1_0_1ze.exe - two chars lowercase`?
+             */
+
+            if (preg_match("/Win32OpenSSL_Light-(\d+\_\d+\_\d+[A-Za-z]*).exe$/i", $node->attr('href'), $matches)) {
+
+                // the version match contains underscores. so we turn "1_0_1d" into "1.0.1d".
+                // the version match might contain uppercase char. so we turn to lowercase for the comparision.
+                // that's still not SemVer, but anyway.
+                $version = strtolower(str_replace('_', '.', $matches[1]));
+
                 if (version_compare($version, $this->registry['openssl']['latest']['version'], '>') === true
-                || (strcmp($this->registry['openssl']['latest']['version'], $version) < 0) ) {
+                    || strcmp($this->registry['openssl']['latest']['version'], $version) < 0) {
                     return array(
-                        'version' => $version,
+                        'version' => $matches[1],
                         'url' => 'http://slproweb.com/download/Win32OpenSSL_Light-'.$matches[1].'.exe'
                     );
                 }

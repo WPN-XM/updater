9f90925370c7187c76505aaae621c2541426f5e4
jakoch <jakoch@web.de>
2015-01-25 12:43:39 +0100

    no IE support - dead horse.

--- index.php
+++ index.php
@@ -65,12 +65,6 @@
           color: #006400;
       }
     </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="assets/js/html5shiv.js"></script>
-    <![endif]-->
 
     <link rel="shortcut icon" href="assets/ico/favicon.ico">
   </head>

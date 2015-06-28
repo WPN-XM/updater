ee2edb8f51556a52b02ce07d0e11fb2730e8da35
jakoch <jakoch@web.de>
2015-06-28 15:50:56 +0200

    added redis crawler, issue https://github.com/WPN-XM/WPN-XM/issues/424

---
+++ crawlers/Redis.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+/**
+ * Version Crawler for Redis
+ *
+ * Website: http://redis.io/
+ * Github:  https://github.com/antirez/redis
+ *
+ * The windows port is maintained by MSOpenTech over at
+ * Github:  https://github.com/MSOpenTech/redis
+ */
+class Redis extends VersionCrawler
+{
+    public $url = 'https://github.com/MSOpenTech/redis/releases/latest';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            /**
+             * The download URL for a file looks like this:
+             * https://github.com/MSOpenTech/redis/releases/download/win-2.8.21/redis-2.8.21.zip
+             */
+            if (preg_match("#download/win-(\d+\.\d+.\d+)/redis-(\d+\.\d+.\d+).zip#", $node->attr('href'), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['redis']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url' => 'https://github.com/MSOpenTech/redis/releases/download/win-' . $version . '/redis-' . $version . '.zip',
+                    );
+                }
+            }
+        });
+    }
+}

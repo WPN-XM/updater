9e55c548258c92841a5a7a127f564c4af49aaa11
jakoch <jakoch@web.de>
2015-09-01 22:41:12 +0200

    added crawler for HeidiSQL, issue https://github.com/WPN-XM/WPN-XM/issues/170

---
+++ src/crawler/HeidiSql.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+/**
+ * HeidiSQL - Version Crawler
+ */
+class HeidiSql extends VersionCrawler
+{
+    public $url = 'http://www.heidisql.com/download.php';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+
+            // http://www.heidisql.com/downloads/releases/HeidiSQL_9.3_Portable.zip
+
+            if (preg_match("#HeidiSQL_(\d+\.\d+(\.\d+)*)_Portable.zip#", $node->attr('href'), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['heidisql']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url'     => 'http://www.heidisql.com/downloads/releases/HeidiSQL_' . $version . '_Portable.zip',
+                    );
+                }
+            }
+        });
+    }
+}

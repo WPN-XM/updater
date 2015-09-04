20f09eccf992f4d00e0a9d692f83c84046e6c89a
jakoch <jakoch@web.de>
2015-09-04 23:27:06 +0200

    added crawler for PhantomJS, issue https://github.com/WPN-XM/WPN-XM/issues/347

---
+++ src/crawler/PhantomJS.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+/**
+ * PhantomJS - Version Crawler
+ */
+class PhantomJS extends VersionCrawler
+{
+    public $url = 'https://bitbucket.org/ariya/phantomjs/downloads/';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#phantomjs-(\d+\.\d+(\.\d+)*)-windows.zip#", $node->attr('href'), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['phantomjs']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url'     => 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-' . $version . '-windows.zip',
+                    );
+                }
+            }
+        });
+    }
+}

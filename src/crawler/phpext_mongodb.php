6f4f44a82321c6e5a39d37bb6add4b7887a5e678
jakoch <jakoch@web.de>
2015-11-04 14:56:01 +0100

    added crawler for phpext_mongodb, issue https://github.com/WPN-XM/WPN-XM/issues/505

---
+++ src/crawler/phpext_mongodb.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+
+/**
+ * PHP Extension for MongoDB - Version Crawler
+ * 
+ * The purpose of this driver is to provide exceptionally thin glue between MongoDB
+ * and PHP, implementing only fundemental and performance-critical components
+ * necessary to build a fully-functional MongoDB driver.
+ * 
+ * Website: https://pecl.php.net/package/mongodb
+ * Github:  http://mongodb.github.io/mongo-php-driver
+ */
+class phpext_mongodb extends VersionCrawler
+{
+    public $url = 'http://windows.php.net/downloads/pecl/releases/mongodb/';
+
+    private $url_template = 'http://windows.php.net/downloads/pecl/releases/mongodb/%version%/php_mongodb-%version%-%phpversion%-nts-%compiler%-%bitsize%.zip';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#(\d+\.\d+(\.\d+)*)$#", $node->text(), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['phpext_mongodb']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url'     => $this->createPhpVersionsArrayForExtension($version, $this->url_template),
+                    );
+                }
+            }
+        });
+    }
+}

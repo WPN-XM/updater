cce19d7eb63809177da824c758c5f22a1f8fa4f9
jakoch <jakoch@web.de>
2015-11-04 19:25:33 +0100

    fix regexp for Sphinx crawlers, issue https://github.com/WPN-XM/WPN-XM/issues/506

--- src/crawler/sphinx_x64.php
+++ src/crawler/sphinx_x64.php
@@ -27,21 +27,21 @@ class sphinx_x64 extends VersionCrawler
     /**
      * There are multiple builds available, but we crawl the following one:
      * "Win32 binaries w/MySQL+PgSQL+libstemmer+id64 support"
-     * 
+     *
      * Direct Download URL: http://sphinxsearch.com/files/sphinx-2.2.10-release-win32-full.zip
-     * 
+     *
      * We grab the version number from the URL leading to the "Thank You" exit page.
      * http://sphinxsearch.com/downloads/sphinx-2.2.10-release-win32-full.zip/thankyou.html
      */
     public function crawlVersion()
     {
-        return $this->filter('a')->each(function ($node) {           
-            if (preg_match("/sphinx-(\d+\.\d+\.\d+[A-Za-z]*)-release-win32-full.zip$/i", $node->attr('href'), $matches)) {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#sphinx-(\d+\.\d+\.\d+[A-Za-z]*)-release-win64-full.zip#i", $node->attr('href'), $matches)) {
                 $version = $matches[1];
                 if (version_compare($matches[1], $this->registry['sphinx']['latest']['version'], '>=') === true) {
                     return array(
                         'version' => $version,
-                        'url'     => 'http://sphinxsearch.com/files/sphinx-' . $version . '-release-win32-full.zip',
+                        'url'     => 'http://sphinxsearch.com/files/sphinx-' . $version . '-release-win64-full.zip',
                     );
                 }
             }

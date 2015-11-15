47a18d5714c4bc7848f411dc4816b2a471212537
jakoch <jakoch@web.de>
2015-11-16 00:33:04 +0100

    added crawler for phpext_couchbase

---
+++ src/crawler/phpext_couchbase.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+/**
+ * Couchbase (PHP Extension) - Version Crawler
+ */
+class phpext_couchbase extends VersionCrawler
+{
+    public $url = 'http://windows.php.net/downloads/pecl/releases/couchbase/';
+
+    private $url_template = 'http://windows.php.net/downloads/pecl/releases/couchbase/%version%/php_couchbase-%version%-%phpversion%-nts-%compiler%-%bitsize%.zip';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#(\d+\.\d+(\.\d+)*)$#", $node->text(), $matches)) {
+                $version = $matches[1];
+                if (version_compare($version, $this->registry['phpext_couchbase']['latest']['version'], '>=') === true) {
+                    return array(
+                       'version' => $version,
+                       'url'     => $this->createPhpVersionsArrayForExtension($version, $this->url_template),
+                    );
+                }
+            }
+        });
+    }
+}

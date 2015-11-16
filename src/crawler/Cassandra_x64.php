651ab32688055c94aae4a69af3e7ad941b6ca2ba
jakoch <jakoch@web.de>
2015-11-16 21:11:22 +0100

    added crawler for Apache Cassandra x64 (datastax community edition)

---
+++ src/crawler/Cassandra_x64.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+/**
+ * Apache Cassandra - Version Crawler
+ *
+ * Apache Cassandra is an open source distributed database management system
+ * designed to handle large amounts of data across many commodity servers,
+ * providing high availability with no single point of failure.
+ *
+ * We use the DataStax Community Edition of Apache Cassandra.
+ *
+ * Website:       http://cassandra.apache.org/
+ *                http://www.datastax.com/
+ *                http://www.planetcassandra.org/cassandra/
+ * Downloads:     http://cassandra.apache.org/download/
+ */
+class Cassandra_x64 extends VersionCrawler
+{
+    public $name = 'cassandra-x64';
+
+    // We could scrape the table: http://www.planetcassandra.org/cassandra/
+    // Or scan the folder:
+    public $url = 'http://downloads.datastax.com/community/';
+
+    public function crawlVersion()
+    {
+        // Download URL: http://downloads.datastax.com/community/datastax-community-64bit_2.2.3.msi
+
+        // filter all a href's with "part of string" match (*)
+        return $this->filter('a[href*="datastax-community-64bit"]')->each(function ($node) {
+
+            if (preg_match("#datastax-community-64bit_(\d+\.\d+\.\d+).msi#i", $node->attr('href'), $matches)) {
+                $version = $matches[1];
+            }
+
+            if (version_compare($version, $this->registry['cassandra-x64']['latest']['version'], '>=') === true) {
+                return array(
+                    'version' => $version,
+                    'url'     => 'http://downloads.datastax.com/community/datastax-community-64bit_' . $version . '.msi',
+                );
+            }
+        });
+    }
+}

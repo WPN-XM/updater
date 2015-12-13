7202c3fa3dd06ef05c341294b16340081fcbf452
jakoch <jakoch@web.de>
2015-12-13 18:27:00 +0100

    added crawler for phpext_msgpack, close https://github.com/WPN-XM/WPN-XM/issues/528

---
+++ src/crawler/phpext_msgpack.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+
+/**
+ * PHP Extension MsgPack - Version Crawler
+ *
+ * Website: http://msgpack.org/
+ */
+class phpext_msgpack extends VersionCrawler
+{
+    public $url = 'http://windows.php.net/downloads/pecl/releases/msgpack/';
+
+    private $url_template = 'http://windows.php.net/downloads/pecl/releases/msgpack/%version%/php_msgpack-%version%-%phpversion%-nts-%compiler%-%bitsize%.zip';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#(\d+\.\d+(\.\d+)*)#", $node->text(), $matches)) {
+                $version = $matches[0];
+
+                if (version_compare($version, $this->registry['phpext_msgpack']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url'     => $this->createPhpVersionsArrayForExtension($version, $this->url_template),
+                    );
+                }
+            }
+        });
+    }
+}

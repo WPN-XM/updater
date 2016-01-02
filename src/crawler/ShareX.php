2978b84dd7c2b4d34cc2a14792b81c6368340364
jakoch <jakoch@web.de>
2016-01-02 11:35:06 +0100

    added crawler for ShareX - https://github.com/ShareX/ShareX

---
+++ src/crawler/ShareX.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+/**
+ * ShareX - Version Crawler
+ *
+ * ShareX is a free and open source program that
+ * lets you capture or record any area of your screen
+ * and share it with a single press of a key.
+ *
+ * It also allows uploading images, text or other types of files
+ * to over 50 supported destinations you can choose from.
+ *
+ * Website: https://getsharex.com
+ * Github:  https://github.com/ShareX/ShareX
+ */
+class ShareX extends VersionCrawler
+{
+    public $name = 'ShareX';
+
+    public $url = 'https://github.com/ShareX/ShareX/releases/latest';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node)
+        {
+            // https://github.com/ShareX/ShareX/releases/download/v10.5.0/ShareX-portable.zip
+
+            if (preg_match("#/releases/download/v(\d+\.\d+.\d+)/#", $node->attr('href'), $matches))
+            {
+                $version = $matches[1];
+
+                $download_file = 'https://github.com/ShareX/ShareX/releases/download/v' . $version . '/ShareX-portable.zip';
+
+                if (version_compare($version, $this->registry['sharex']['latest']['version'], '>=') === true) {
+                    return array(
+                        'version' => $version,
+                        'url'     => $download_file,
+                    );
+                }
+            }
+        });
+    }
+}

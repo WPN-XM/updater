be7f0519e8e9744004683c8ddb0a3f9024b3dde4
jakoch <jakoch@web.de>
2016-01-09 18:14:11 +0100

    fix error display

--- src/view/RegistryHealthCheck.php
+++ src/view/RegistryHealthCheck.php
@@ -5,6 +5,7 @@ if(count($health) === 0) {
 	echo '<span class="label label-success">OK</span>';
 } else {
 	$errorMsg = '<span class="label label-danger">ERROR</span> ';
+    echo $errorMsg;
     echo implode($health, '<br/>' . $errorMsg);
 }
 ?>

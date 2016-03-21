e4b1c574dfb08640d0cc0a329e5975d432510fbe
jakoch <jakoch@web.de>
2016-03-21 16:24:42 +0100

    scrape extensions from PECL, issue https://github.com/WPN-XM/WPN-XM/issues/407

---
+++ src/PHPExtensionScraper.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016, Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater;
+
+use WPNXM\Updater\FileCache;
+
+/**
+ * The purpose of this class is to scrape all available PHP extensions
+ * from the offical PECL server
+ *
+ * This is done by fetching http://windows.php.net/downloads/pecl/releases/
+ * and converting the HTML into a JSON list of all available PHP extensions.
+ */
+class PHPExtensionScraper
+{
+    private function getHtml()
+    {
+        // callback to modify the fetched content, before caching it
+        $modificationCallback = function ($content) {
+            return self::strip($content);
+        };
+
+        $releases = FileCache::get(
+            'http://windows.php.net/downloads/pecl/releases/',
+            DATA_DIR.'pecl-releases.cache.html',
+            $modificationCallback
+        );
+
+        $snaps = FileCache::get(
+            'http://windows.php.net/downloads/pecl/snaps/',
+            DATA_DIR.'pecl-snaps.cache.html',
+            $modificationCallback
+        );
+
+        $html = $releases.$snaps;
+
+        return $html;
+    }
+
+    public static function strip($text)
+    {
+        // strip first two lines
+        $a = explode("\n", $text);
+        unset($a[0], $a[1]);
+        $text = implode($a);
+
+        // strip several strings
+        $text = str_replace(
+            ['<br>', '&lt;dir&gt;', '<pre><A HREF="/downloads/pecl/">[To Parent Directory]</A>'],
+            ['', '', ''],
+            $text
+        );
+
+        return $text;
+    }
+
+    public function scrapeExtensionsHtml($html)
+    {
+        $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+
+        if (preg_match_all("/$regexp/siU", $html, $matches, PREG_SET_ORDER)) {
+            $extensions = [];
+            foreach ($matches as $match) {
+                // $match[2] = link address
+                // $match[3] = link text
+                $extensions[] = $match[3];
+            }
+        }
+
+        $extensions = array_unique($extensions);
+
+        sort($extensions);
+
+        return $extensions;
+    }
+
+    public function getJson()
+    {
+        return json_encode($this->scrapeExtensionsHtml($this->getHtml()));
+    }
+}
---
+++ src/action/UpdatePHPExtensionList.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Action;
+
+use WPNXM\Updater\ActionBase;
+use WPNXM\Updater\PHPExtensionScraper;
+
+/**
+ * Fetches all PHP extension names from PECL and save it as a JSON file.
+ */
+class UpdatePhpExtensionList extends ActionBase
+{
+    public function __invoke()
+    {
+        $this->scraper = new PHPExtensionScraper;
+
+        return (bool) file_put_contents(
+            DATA_DIR . 'registry/php-extensions-on-pecl.json',
+            $this->scraper->getJson()
+        );
+    }
+}

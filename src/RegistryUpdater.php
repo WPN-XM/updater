48e3aba8b3dae124a5ea597818d352c320f90999
jakoch <jakoch@web.de>
2016-05-21 01:21:16 +0200

    updated RegistryUpdater to work with Guzzle 6 close https://github.com/WPN-XM/WPN-XM/issues/541

--- src/RegistryUpdater.php
+++ src/RegistryUpdater.php
@@ -12,16 +12,21 @@
 namespace WPNXM\Updater;
 
 use Goutte\Client as GoutteClient;
+
 use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 
 class RegistryUpdater
 {
+    /**
+     * @object \GuzzleHttp\Client
+     */
     public $guzzleClient;
-    public $crawlers     = array();
-    public $urls         = array();
-    public $results      = array();
-    public $registry     = array();
-    public $old_registry = array();
+    public $urls         = [];
+    public $registry     = [];
+    public $old_registry = [];
+    public $html         = '';
 
     const USER_AGENT = 'WPN-XM Server Stack - Software Registry Update Tool - http://wpn-xm.org/';
 
@@ -29,23 +34,16 @@ class RegistryUpdater
     {
         $this->registry     = $registry;
         $this->old_registry = $registry;
-    }
 
-    public function setupCrawler()
-    {
-        // init Goutte and set header for all requests
-        $goutteClient = new GoutteClient;
-        $goutteClient->setHeader('User-Agent', RegistryUpdater::USER_AGENT);
-
-        // fetch Guzzle out of Goutte and deactivate SSL Verification
-        $this->guzzleClient = $goutteClient->getClient();
-        $this->guzzleClient->setDefaultOption('verify', false);
-
-        $goutteClient->setClient($this->guzzleClient);
+        // Setup Guzzle Client and set header and curl options for all requests
+        $this->guzzleClient = new \GuzzleHttp\Client([
+            'headers' => [ 'User-Agent' => RegistryUpdater::USER_AGENT ],           
+            'curl' => [ CURLOPT_SSL_VERIFYPEER => false ]   
+        ]);
     }
 
     /**
-     * Returns array with one or more crawler file names.
+     * Return array with one or more crawler file names.
      *
      * @param string $component
      * @return array
@@ -62,25 +60,27 @@ class RegistryUpdater
         return glob(__DIR__ . '\crawler\*.php');
     }
 
+    /**
+     * @return int The number of URLs to crawl.
+     */
     public function getUrlsToCrawl($single_component = null)
     {
         $crawlers = $this->getCrawlers($single_component);
 
-        $i = 0;
-
         foreach ($crawlers as $i => $file) {
 
-            // instantiate version crawler
+            // load and instantiate version crawler
             include $file;
-            $component = str_replace(array('-', '.'), array('_', '_'), strtolower(pathinfo($file, PATHINFO_FILENAME)));
+            $component = str_replace(array('-', '.'), array('_', '_'), strtolower(pathinfo($file, PATHINFO_FILENAME)));        
             $classname = 'WPNXM\Updater\Crawler\\' . ucfirst($component);
-            $crawler   = new $classname();
+            $crawler = new $classname();
 
-            // use the registry shorthand from the crawler and the component as fallback
-            $registryShorthand = isset($crawler->name) ? $crawler->name : $component;
+            // use the registry key from the crawler or the component as fallback
+            // TODO find out why $component is needed here (which crawlers do not have a name?)
+            $registryKey = isset($crawler->name) ? $crawler->name : $component;
 
-            // set registry and component name to crawler
-            $crawler->setRegistry($this->registry, $registryShorthand);
+            // set "software registry" and "component name" to crawler
+            $crawler->setRegistry($this->registry, $registryKey);
 
             // store crawler object in crawlers array
             $this->crawlers[$i] = $crawler;
@@ -89,121 +89,145 @@ class RegistryUpdater
             $this->urls[] = $crawler->getURL();
         }
 
-        return $i + 1;
+        return count($this->urls); 
+    }
+
+    public function getTotalNumberOfRequests()
+    {
+        return count($this->urls);
     }
 
     /**
-     * Crawl launches several URL requests in parallel.
-     * The response time will be the time of the longest request.
+     * Crawl several URLs in parallel.
      */
     public function crawl()
     {
-        $requests = array();
+        $requests = [];
+        
+        // Prepare Requests Closure.
+        // The closure accepts the URLs array, counts the total number of URLs 
+        // and creates new Requests for each URL.
+        $requests = function (array $urls) {
+            $total_num_requests = $this->getTotalNumberOfRequests();
+            for ($i = 0; $i < $total_num_requests; $i++) {
+                yield new Request('GET', $urls[$i]);
+            }
+        };
 
-        foreach ($this->urls as $idx => $url) {
-            // guzzle does not accept an array of URLs anymore
-            // now Urls must be objects implementing the \GuzzleHttp\Message\RequestInterface
-            $requests[] = $this->guzzleClient->createRequest('GET', $url, ['allow_redirects' => true]);
-        }
+        // Setup Request Pool.
+        // The Requests Closure is inserted as parameter of the Pool.
+        $pool = new Pool($this->guzzleClient, $requests($this->urls), [
+            'concurrency' => 5,
+            'fulfilled' => function ($response, $index) {
+                // this is delivered each successful response
+                $this->fulfilledResponse($response, $index);
+            },
+            'rejected' => function ($reason, $index) {
+                // this is delivered each failed request
+                $this->rejectedResponse($reason, $index);
+            },
+        ]);
 
-        // results is a GuzzleHttp\BatchResults object
-        $this->results = \GuzzleHttp\Pool::batch($this->guzzleClient, $requests);
+        // Initiate the transfers and create a promise.
+        $promise = $pool->promise();
+
+        // Wait for Pool of Requests to complete.
+        $promise->wait();
     }
 
-    public function evaluateResponses()
+    /**
+     * Handler Function for a failed Requests (Reason).
+     */
+    public function rejectedResponse($reason, $index)
     {
-        $html = '';
-        $i    = 0;
-
-        // Retrieve all failures.
-        foreach ($this->results->getFailures() as $requestException) {
-            echo $requestException->getMessage() . "\n";
-        }
-
-        // Retrieve all successful responses
-        // iterate through responses and insert them in the crawler objects
-        foreach ($this->results->getSuccessful() as $response) {
-            $new_version = $old_version = '';
-
-            // set the response to the version crawler object
-            $this->crawlers[$i]->addContent($response->getBody(), $response->getHeader('Content-Type'));
-
-            $component     = $this->crawlers[$i]->getName();
-            $latestVersion = $this->crawlers[$i]->crawlVersion();
-
-            if($latestVersion === null) {
-                echo '[Crawling Error] Version Scan for Component "'.$component.'" returned no version.';
-                continue;
-            }
-
-            $latestVersion = ArrayUtil::clean($latestVersion);
-
-            /**
-             * Add (new) latest version (array) for this component to the registry.
-             */
-            $this->registry = Registry::addLatestVersionToRegistry($component, $latestVersion, $this->old_registry);
-
-            /**
-             * onAfterVersionInsert Event
-             *
-             * This event allows executing custom functionality after the version was inserted.
-             * One might apply further changes, like rewriting the registry, for instance,
-             * to rewrite and update old URLs, when file movements of the download files occured.
-             * For example, like "PHP" moves old versions into the download "/archives" folder.
-             */
-            if(method_exists($this->crawlers[$i], 'onAfterVersionInsert') === true) {
-                $this->registry = $this->crawlers[$i]->onAfterVersionInsert($this->registry);
-            }
-
-            // get old and new version for comparison.
-
-            /**
-             * get old version
-             * use 0.0.0, in case the component is not in the registry, yet (newly added crawler)
-             */
-            $old_version = isset($this->old_registry[$component]['latest']['version'])
-                ? $this->old_registry[$component]['latest']['version']
-                : '0.0.0';
-
-
-            $new_version = $this->registry[$component]['latest']['version'];
-
-            /**
-             * Latest Version
-             */
-            if (Version::compare($component, $old_version, $new_version) === true) {
-                // write a temporary component registry, for later registry insertion
-                Registry::writeRegistrySubset($component, $this->registry[$component]);
-
-                // render a table row (version comparison display)
-                $html .= ViewHelper::renderTableRow($component, $old_version, $new_version, true);
-            }
-            /**
-             * Missing version
-             */
-            elseif (Version::notInRegistry($latestVersion, $this->old_registry[$component]) === true)
-            {
-                // write a temporary component registry, for later registry insertion
-                Registry::writeRegistrySubset($component, $this->registry[$component]);
-
-                // check, if this is missing version number
-                $new_version = Version::notInRegistry($latestVersion, $this->old_registry[$component], true);
-
-                // render a table row (version comparison display)
-                $html .= ViewHelper::renderTableRow($component, $old_version, $new_version, true);
-            } else {
-                // render a table row (version comparison display)
-                $html .= ViewHelper::renderTableRow($component, $old_version, $new_version, false);
-            }
-
-            $i++;
-        }
-
-        return $html;
+        //var_dump($reason, $index);
     }
 
-    public function setRegistry($registry)
+    /**
+     * Handler Function for a successful Request (Response).
+     */
+    public function fulfilledResponse($response, $index)
     {
-        $this->registry = $registry;
+        /**
+         * Set "response" to the "version crawler" object.
+         * We use Symfony/DomCrawler->addContent() to set the content to scrape.
+         */
+        $body        = $response->getBody();
+        $contentType = $response->getHeader('Content-Type')[0];
+        $this->crawlers[$index]->addContent($body, $contentType);
+
+        $component     = $this->crawlers[$index]->getName();
+        $latestVersion = $this->crawlers[$index]->crawlVersion(); // scrape version from content
+
+        if($latestVersion === null) {
+            echo '[Crawling Error] Version Scan for Component "'.$component.'" returned no version.';
+            return;
+        }
+
+        $latestVersion = ArrayUtil::clean($latestVersion);
+
+        /**
+         * Add (new) latest version (array) for this component to the registry.
+         */
+        $this->registry = Registry::addLatestVersionToRegistry($component, $latestVersion, $this->old_registry);
+
+        /**
+         * onAfterVersionInsert Event
+         *
+         * This event allows executing custom functionality after the version was inserted.
+         * One might apply further changes, like rewriting the registry, for instance,
+         * to rewrite and update old URLs, when file movements of the download files occured.
+         * For example, like "PHP" moves old versions into the download "/archives" folder.
+         */
+        if(method_exists($this->crawlers[$index], 'onAfterVersionInsert') === true) {
+            $this->registry = $this->crawlers[$index]->onAfterVersionInsert($this->registry);
+        }
+
+        /**
+         * Get "old version" and "new version" for comparison.
+         *
+         * Use "old version" defaults to "0.0.0", in case the component is not in the registry, yet.
+         * This happens only, when it is a newly added crawler.
+         */
+        $new_version = $this->registry[$component]['latest']['version'];
+
+        $old_version = isset($this->old_registry[$component]['latest']['version'])
+            ? $this->old_registry[$component]['latest']['version']
+            : '0.0.0';        
+
+        /**
+         * Latest Version
+         */
+        if (Version::compare($component, $old_version, $new_version) === true) {
+            // write a temporary component registry, for later registry insertion
+            Registry::writeRegistrySubset($component, $this->registry[$component]);
+
+            // render a table row (version comparison display)
+            $this->html .= ViewHelper::renderTableRow($component, $old_version, $new_version, true);
+        }
+        /**
+         * Missing version
+         *
+         * TODO why are here 2 calls to Version::notInRegistry()?
+         */
+        elseif (Version::notInRegistry($latestVersion, $this->old_registry[$component]) === true)
+        {
+            // write a temporary component registry, for later registry insertion
+            Registry::writeRegistrySubset($component, $this->registry[$component]);
+
+            // check, if this is a missing version number
+            $new_version = Version::notInRegistry($latestVersion, $this->old_registry[$component], true);
+
+            // render a table row (version comparison display)
+            $this->html .= ViewHelper::renderTableRow($component, $old_version, $new_version, true);
+        } else {
+            // render a table row (version comparison display)
+            $this->html .= ViewHelper::renderTableRow($component, $old_version, $new_version, false);
+        }
+    }
+
+    public function getHtmlTable()
+    {
+        return $this->html;
     }
 }

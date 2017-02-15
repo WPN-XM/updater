e4461869f1d049dfe668eae20b773dcb48763a8b
jakoch <jakoch@web.de>
2017-02-15 22:08:10 +0100

    update crawler for phpext_sqlsrv, issue https://github.com/WPN-XM/WPN-XM/issues/550

--- src/crawler/phpext_sqlsrv.php
+++ src/crawler/phpext_sqlsrv.php
@@ -2,7 +2,7 @@
 
 /**
  * WPИ-XM Server Stack - Updater
- * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * Copyright © 2010 - 2017 Jens A. Koch <jakoch@web.de>
  * http://wpn-xm.org/
  *
  * This source file is subject to the terms of the MIT license.
@@ -18,26 +18,23 @@ use WPNXM\Updater\VersionCrawler;
  *
  * Website:   https://github.com/Azure/msphpsql
  * Downloads: https://github.com/Azure/msphpsql/releases
+ * PECL:      https://pecl.php.net/package/sqlsrv
  */
 class phpext_sqlsrv extends VersionCrawler
 {
-    public $url = 'https://github.com/Azure/msphpsql/releases';
+    public $url = 'http://windows.php.net/downloads/pecl/releases/sqlsrv/';
 
-    private $url_template = 'https://github.com/Azure/msphpsql/releases/download/v%version%/php_sqlrv_%version%_%bitsize%.zip';
+    private $url_template = 'http://windows.php.net/downloads/pecl/releases/sqlsrv/%version%/php_sqlsrv-%version%-%phpversion%-nts-%compiler%-%bitsize%.zip';
 
     public function crawlVersion()
     {
         return $this->filter('a')->each(function ($node) {
+            if (preg_match("#(\d+\.\d+(\.\d+)*)#", $node->text(), $matches)) {
+                $version = $matches[0];
 
-            // https://github.com/Azure/msphpsql/releases/download/v4.0.4/php_sqlrv_4.0.4_x86.zip
-
-            if (preg_match("#Azure\/msphpsql\/releases\/download\/v(\d+\.\d+\.\d+)#i", $node->attr('href'), $matches)) {
-                $version = $matches[1];
-
-                if (version_compare($version, $this->registry['phpext_sqlsrv']['latest']['version'], '>=') === true)  {
+                if (version_compare($version, $this->registry['phpext_solr']['latest']['version'], '>=') === true)  {
 
                     $urls = $this->createPhpVersionsArrayForExtension($version, $this->url_template);
-
                     if(empty($urls)) {
                         return;
                     }
@@ -49,47 +46,5 @@ class phpext_sqlsrv extends VersionCrawler
                 }
             }
         });
-    }
-
-    /**
-     * Creates the version array for PHP Extension "sqlsrv".
-     * 
-     * I have to support a custom naming scheme for this extension,
-     * referencing: https://github.com/Azure/msphpsql/issues/88
-     *
-     * php_sqlrv_4.0.4_x64.zip 
-     * php_sqlrv_4.0.4_x86.zip
-     * = php_sqlrv_%version%_%bitsize%.zip
-     *
-     * @param  string $version       Version.
-     * @param  string $url           PHP Extension URL with placeholders.
-     * @param  bool   $skipURLcheck
-     * @return array
-     */
-    public function createPhpVersionsArrayForExtension($version, $url, $skipURLcheck = false)
-    {
-        $url = str_replace("%version%", $version, $url);
-
-        $bitsizes    = array('x86', 'x64');
-
-        // WARNING!
-        // The PHP version is only available inside the zip.
-        // https://github.com/Azure/msphpsql/releases/download/v4.0.4/php_sqlrv_4.0.4_x86.zip
-        // Update the array manually, until the naming scheme is changed...
-        $phpversions = array('7.0.0');
-
-        $urls        = array();
-        foreach ($bitsizes as $bitsize) {
-            foreach ($phpversions as $phpversion) {
-
-                $replacedUrl = str_replace('%bitsize%', $bitsize, $url);
-
-                $urls[$bitsize][$phpversion] = $replacedUrl;
-            }
-        }
-
-        //var_dump($urls); exit;
-
-        return $urls;
     }
 }

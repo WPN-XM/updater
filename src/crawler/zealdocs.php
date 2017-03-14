241a08dd200c271a7aae7ed377dbe162fa6881b0
jakoch <jakoch@web.de>
2017-03-14 16:36:23 +0100

    added crawler for ZealDocs, issue https://github.com/WPN-XM/WPN-XM/issues/494

---
+++ src/crawler/zealdocs.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+
+/**
+ * ZealDocs - Version Crawler
+ * 
+ * Website:     http://zealdocs.org/
+ * Downloads:   https://bintray.com/zealdocs/windows/zeal
+ */
+class zealdocs extends VersionCrawler
+{
+    // we are scraping the bintray downloads page
+    public $url = 'https://dl.bintray.com/zealdocs/windows';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+                if (preg_match("#zeal-portable-(\d+\.\d+.\d+)-windows-x86.7z#", $node->text(), $matches)) {
+                    $version = $matches[1];
+                    if (version_compare($version, $this->registry['zealdocs']['latest']['version'], '>=') === true) {
+                        return array(
+                            'version' => $version,
+                            // https://dl.bintray.com/zealdocs/windows/:zeal-portable-0.3.1-windows-x86.7z
+                            'url'     => 'https://dl.bintray.com/zealdocs/windows/:zeal-portable-' . $version . '-windows-x86.7z',
+                        );
+                    }
+                }
+            });
+    }
+}

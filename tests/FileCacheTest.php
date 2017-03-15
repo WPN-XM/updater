2ad9a0877477d4a5500d84a6a4ec983d4e5dc7e2
jakoch <jakoch@web.de>
2017-03-15 06:27:59 +0100

    add FileCacheTest

---
+++ tests/FileCacheTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace tests;
+
+use WPNXM\Updater\FileCache;
+
+class FileCacheTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->cacheFile = __DIR__.'\phpunit.xml.dist.cache';
+    }
+    
+    protected function tearDown()
+    {
+        unlink($this->cacheFile);
+    }
+
+    public function testGet()
+    {
+        // callback to modify the fetched content, before caching it
+        $modificationCallback = function ($content) {
+            return str_replace('phpunit', 'modified-string', $content);
+        };
+
+        $result = FileCache::get(
+            __DIR__.'\phpunit.xml.dist',
+            $this->cacheFile,
+            $modificationCallback
+        );
+
+        $this->assertContains('modified-string', $result);
+        $this->assertContains('convertErrorsToExceptions', $result);
+    }
+}

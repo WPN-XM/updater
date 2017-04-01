70f4be6533d6a3ca387288239fa6cf9d723c9d97
jakoch <jakoch@web.de>
2017-04-01 17:40:40 +0200

    list new crawlers (without registry) on top of overview table so that i can easily do a version scan or add to the registry

--- src/action/Overview.php
+++ src/action/Overview.php
@@ -14,15 +14,20 @@ namespace WPNXM\Updater\Action;
 use WPNXM\Updater\ActionBase;
 use WPNXM\Updater\View;
 use WPNXM\Updater\Registry;
+use WPNXM\Updater\VersionCrawlers;
 
 class Overview extends ActionBase
 {
     public function __invoke()
-    {
+    {        
+        $registry = Registry::load();        
+        $crawlers = VersionCrawlers::findCrawlersWithoutRegistryEntry($registry);
+       
         /* View */
 
         $view                    = new View();
-        $view->data['registry']  = Registry::load();
+        $view->data['registry']  = $registry;
+        $view->data['crawlers']  = $crawlers;
         $view->render();
-    }
+    } 
 }
--- src/view/Overview.php
+++ src/view/Overview.php
@@ -24,6 +24,18 @@
               <thead><tr><th style="width: 220px">Software Component</th><th>Version</th><th>Action</th></tr></thead>
               <tbody>
                 <?php 
+                    if($crawlers) { 
+                      foreach ($crawlers as $name) {
+                          echo '<tr>';
+                          echo '<td><span class="badge badge-danger">New Crawler</span> ' . $name . '</td>';   
+                          echo '<td>Not in registry, yet.</td>';
+                          echo '<td><a class="btn btn-info btn-xs" href="index.php?action=scan-component&amp;component=' . $name . '">Scan</a></td>';
+                          echo '<td><a class="btn btn-info btn-xs" href="index.php?action=add-component&amp;component=' . $name . '">Add Registry Entry</a></td>';
+                          echo '</tr>';
+                      }
+                    }
+                ?>
+                <?php 
                     foreach ($registry as $item => $component) {
                         echo '<tr>';
                         echo '<td>' . $component['name'] . '</td>';   

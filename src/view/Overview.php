9009239f25ae6cba0438b11b29fd186e6bb86bc5
jakoch <jakoch@web.de>
2017-04-01 17:41:47 +0200

    remove superfluous buttons

--- src/view/Overview.php
+++ src/view/Overview.php
@@ -3,21 +3,7 @@
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
             <div class="panel-heading">
-              <span class="glyphicon glyphicon-list"></span>&nbsp; Version Crawler
-              <span class="pull-right">
-                <button class="btn btn-success btn-xs" href="index.php?action=add">
-                  <span class="glyphicon glyphicon-plus"></span>
-                  Add Component
-                </button>
-                <button class="btn btn-info btn-xs" href="index.php?action=update">
-                  <span class="glyphicon glyphicon-search"></span>
-                  Merge Scans into Registry
-                </button>
-                <button class="btn btn-info btn-xs" href="index.php?action=scan-component">
-                  <span class="glyphicon glyphicon-search"></span>
-                  Scan All
-                </button>
-              </span>
+              <span class="glyphicon glyphicon-list"></span>&nbsp; Version Crawler              
             </div>
             <div class="panel-body">
               <table class="table table-sm table-hover table-striped table-bordered" style="font-size: 12px;">

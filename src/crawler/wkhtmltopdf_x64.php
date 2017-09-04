d4d75a51f238ee5270ba73401092b7f4671c64a2
jakoch <jakoch@web.de>
2017-09-04 17:10:52 +0200

    fix wkhtmltopdf crawlers

--- src/crawler/wkhtmltopdf_x64.php
+++ src/crawler/wkhtmltopdf_x64.php
@@ -21,21 +21,20 @@ use WPNXM\Updater\VersionCrawler;
  * These run entirely "headless" and do not require a display or display service.
 
  * Website:   http://wkhtmltopdf.org/
- * Gna:       http://gna.org/projects/wkhtmltopdf
- * Downloads: http://download.gna.org/wkhtmltopdf/
+ * Github:    https://github.com/wkhtmltopdf/wkhtmltopdf
+ * Downloads: https://github.com/wkhtmltopdf/wkhtmltopdf/releases/
  */
 class wkhtmltopdf_x64 extends VersionCrawler
 {
     public $name = 'wkhtmltopdf-x64';
-
-    // we are scraping the gna downloads folder
-    public $url = 'http://download.gna.org/wkhtmltopdf/0.12/';
+    
+    public $url = 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/latest';
 
     public function crawlVersion()
     {
         return $this->filter('a')->each(function ($node) {
 
-                if (preg_match("#(\d+.\d+.\d+(.\d+)?)/#", $node->attr('href'), $matches)) {
+                if (preg_match("#/releases/download/(\d+\.\d+.\d+)/wkhtmltox-(.*)_msvc2015-win64.exe#", $node->attr('href'), $matches)) {
                     $version = $matches[1];
 
                     /**
@@ -43,9 +42,11 @@ class wkhtmltopdf_x64 extends VersionCrawler
                      * - http://download.gna.org/wkhtmltopdf/0.12/0.12.3.2/wkhtmltox-0.12.3.2_msvc2013-win32.exe
                      * - http://download.gna.org/wkhtmltopdf/0.12/0.12.3.2/wkhtmltox-0.12.3.2_msvc2013-win64.exe
                      *
-                     * From v0.12.4 "msvc2015" is used.
+                     * From v0.12.4 "msvc2015" is used. Now also SemVer. Yippie.
+                     * - https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_msvc2015-win64.exe
                      */
-                    $download_file = 'http://download.gna.org/wkhtmltopdf/0.12/' . $version . '/wkhtmltox-' . $version . '_msvc2015-win64.exe';
+                    $download_file = 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/';
+                    $download_file .= $version . '/wkhtmltox-' . $version . '_msvc2015-win64.exe';
 
                     if (version_compare($version, $this->latestVersion, '>=') === true) {
                         return array(

59ae1ab487ccd18d5c1789530f4c42d49fcf758f
jakoch <jakoch@web.de>
2017-09-12 00:26:57 +0200

    show link status of a component - with button "remove link" to delete invalid/red/404 links manually

--- src/view/LinkStatusComponent.php
+++ src/view/LinkStatusComponent.php
@@ -1,11 +1,22 @@
 <h5>WPN-XM Software Registry - Status <span class="pull-right"><?=date(DATE_RFC822)?></span></h5>
-<h5>For Component "<?=$component?>"</h5>
+<h5>For Component "<?=$software?>"</h5>
 <table class="table table-sm table-hover" style="font-size: 12px;">
 <tr><th>Download URLs</th></tr>
 <?php
 foreach($urlsHttpStatus as $url => $status) {
-	$color = ($status === true) ? 'green' : 'red';
-	echo '<tr><td><a style="color:' . $color . ';" href="' . $url . '">' . $url . '</a></td></tr>';
+    $color = ($status === true) ? 'green' : 'red';
+    echo '<tr>';
+    echo '  <td><a style="color:' . $color . ';" href="' . $url . '">' . $url . '</a></td>';
+    if($status === false) {
+        echo '  <td>';
+        echo '    <form action="index.php?action=link-remove-component&amp;software=' . $software . '" method="POST" name="removeLink">';
+        echo '      <input type="hidden" name="software" value="' . $software . '">';
+        echo '      <input type="hidden" name="url" value="' . urlencode($url) . '">';
+        echo '      <a class="btn btn-alert btn-xs" href="#" onclick="document.removeLink.submit();">Remove Link</a>';
+        echo '    </form>';
+        echo '  </td>';
+    }
+    echo '</tr>';
 }
 ?>
 </table>

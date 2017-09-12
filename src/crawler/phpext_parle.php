bc29d3b21c297c640cbcd2bf67b8aeddab92fe8c
jakoch <jakoch@web.de>
2017-09-12 20:46:59 +0200

    add crawler for phpext_parle

---
+++ src/crawler/phpext_parle.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright (c) Jens A. Koch <jakoch@web.de>
+ * https://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Updater\Crawler;
+
+use WPNXM\Updater\VersionCrawler;
+
+
+/**
+ * parle (PHP Extension) - Version Crawler
+ */
+class phpext_parle extends VersionCrawler
+{
+	public $name = 'phpext_parle';
+	
+    public $url = 'http://windows.php.net/downloads/pecl/releases/parle/';
+
+    private $url_template = 'http://windows.php.net/downloads/pecl/releases/parle/%version%/php_parle-%version%-%phpversion%-nts-%compiler%-%bitsize%.zip';
+
+    public function crawlVersion()
+    {
+        return $this->filter('a')->each(function ($node) {
+            if (preg_match("#(\d+\.\d+(\.\d+)*)$#", $node->text(), $matches)) {
+                $version = $matches[1];
+
+                if (version_compare($version, $this->latestVersion, '>=') === true) {
+					
+					$urls = $this->createPhpVersionsArrayForExtension($version, $this->url_template);
+					if(empty($urls)) {
+						return;
+					}
+				
+                    return array(
+                        'version' => $version,
+                        'url'     => $urls,
+                    );
+                }
+            }
+        });
+    }
+}

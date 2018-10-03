c40ee0ea5839620fe9afae8118fb4ae03a96e099
jakoch <jakoch@web.de>
2018-10-03 23:47:04 +0200

    use new crawling endpoint for curl

--- src/crawler/curl_x86.php
+++ src/crawler/curl_x86.php
@@ -18,47 +18,34 @@ use WPNXM\Updater\VersionCrawler;
  *
  * Website: https://curl.haxx.se/
  * Github:  https://github.com/curl/curl
- *
- * Builds by Viktor Szakats
- * Github:               https://github.com/vszakats/harbour-deps
- * Downloads on Bintray: https://bintray.com/vszakats/generic/curl/#files
+ * Windows Builds Github: https://github.com/curl/curl-for-win
  */
 class curl_x86 extends VersionCrawler
 {
     public $name = 'curl-x86';
 
-    /**
-     * we could scrape https://bintray.com/vszakats/generic/curl/#files
-     * or https://dl.bintray.com/vszakats/generic/
-     * but bintray is cool and provides an API including "latest_version", which is superb!
-     */
-    public $url = 'https://api.bintray.com/packages/vszakats/generic/curl';
+    public $url = 'https://curl.haxx.se/windows/';
 
     public function crawlVersion()
     {
-        $version = json_decode(file_get_contents($this->url), true)['latest_version'];
-
-        /**
-         * Downloads are on Bintray
-         *
-         * API for Downloading Content:
-         * https://bintray.com/docs/api/#_download_content
-         *
-         * DL URL:
-         * https://dl.bintray.com/vszakats/generic/curl-7.47.1-win32-mingw.7z
-         */
-        $download_file = 'https://dl.bintray.com/vszakats/generic/curl-' . $version . '-win32-mingw.7z';
-
-        // the file exists check is needed, because we don't know
-        // if a libressl version was build for latest version number
-        if($this->fileExistsOnServer($download_file) === true)
+        return $this->filter('a')->each(function ($node)
         {
-            if (version_compare($version, $this->latestVersion, '>=') === true) {
-                return array(
-                    'version' => $version,
-                    'url'     => $download_file,
-                );
-            }
-        }
+            if (preg_match("#curl-((\d+\.\d+.\d+)(_\d+))-win32-mingw.zip#", 
+                $node->attr('href'), $matches)) 
+            {
+                $vb = $matches[1]; // version_plus_buildVersion
+                $version = $matches[2];
+
+                $download_file = 'https://curl.haxx.se/windows/dl-'.$vb.'/curl-'.$vb.'-win32-mingw.zip';
+
+                if (version_compare($version, $this->latestVersion, '>=') === true) 
+                {
+                    return array(
+                        'version' => $version,
+                        'url'     => $download_file,
+                    );
+                }
+            }            
+        });
     }
 }

1a088096e7ee9a9a02ca52817c4aec2e89c049c2
jakoch <jakoch@web.de>
2018-10-04 17:15:05 +0200

    comment

--- src/crawler/phpext_ice.php
+++ src/crawler/phpext_ice.php
@@ -25,13 +25,13 @@ class phpext_ice extends VersionCrawler
 	
     public $url = 'http://www.iceframework.org/dll/';
 
+    // https://www.iceframework.org/dll/ice-1.3.0-php-7.2-ts-vc15-x86.zip
     private $url_template = 'http://www.iceframework.org/dll/ice-%version%-php-%phpversion%-nts-%compiler%-%bitsize%.zip';
 
     public function crawlVersion()
     {
         return $this->filter('a')->each(function ($node) {
 
-            // ice-1.1.2-php-5.6-nts-vc11-x64.zip
             // ice-1.1.3-php-7.0-nts-vc14-x64.zip
             if (preg_match("/ice-(\d+\.\d+\.\d+)-php-(\d+\.\d+)-nts-vc\d+-x64\.zip/i", $node->attr('href'), $matches)) {
                 $version = $matches[1];

39f9429f1efc63ba44907991431190ac92323938
jakoch <jakoch@web.de>
2018-12-14 18:50:40 +0100

    add index action

---
+++ src/action/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack - Updater
+ * Copyright (c) Jens A. Koch <jakoch@web.de>
+ * https://wpn-xm.org/
+ *
+ * Licensed under the MIT License.
+ * See the bundled LICENSE file for copyright and license information.
+ */
+
+namespace WPNXM\Updater\Action;
+
+use WPNXM\Updater\ActionBase;
+use WPNXM\Updater\View;
+
+class Index extends ActionBase
+{
+    public function __invoke()
+    {
+        $view = new View();
+        $view->render();
+    }
+}

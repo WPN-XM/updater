fed8e89b5075db7bade4333f2747ae3b54fba960
jakoch <jakoch@web.de>
2019-01-04 12:15:55 +0100

    fix css for label => badge

--- src/view/HealthCheckRegistry.php
+++ src/view/HealthCheckRegistry.php
@@ -2,9 +2,9 @@
 
 <?php
 if(count($health) === 0) {
-	echo '<span class="label label-success">OK</span>';
+	echo '<span class="badge badge-success">OK</span>';
 } else {
-	$errorMsg = '<span class="label label-danger">ERROR</span> ';
+	$errorMsg = '<span class="badge badge-danger">ERROR</span> ';
     echo $errorMsg;
     echo implode($health, '<br/>' . $errorMsg);
 }
